040364717bbcc32b22c9fadc860b73c4a61d68df
sincerity628 <14785481094@163.com>
2019-12-14 11:29:32 +0800

    added the trans details of the blocks.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 import BlockCard from '../../components/block-card/BlockCard';
+import TransactionTable from '../../components/transaction-table/TransactionTable';
 import './home.css';
 const { initChain } = require('../../store/store');
 
 const Home = () => {
   const [chain, setChain] = useState(initChain);
   const [blocks, setBlocks] = useState(chain.chain);
+  const [transaction, setTransaction] = useState(blocks[0].transaction);
+  const [clickedIndex, setClickedIndex] = useState(0);
+
+  const handleClick = (index) => {
+    setClickedIndex(index);
+    setTransaction(blocks[index].transaction);
+  }
 
   const handleWarning = () => {
     setChain(initChain);
@@ -20,9 +28,23 @@ const Home = () => {
       <div className="container px-0 card-container">
         { blocks.map((block, index) => {
           return (
-            <BlockCard block={block} index={index} key={block.hash} />
+            <div onClick={() => { handleClick(index) }}
+              key={block.hash}
+              className={index === clickedIndex? 'click-card clicked-card' : 'click-card'}
+            >
+              <BlockCard block={block} index={index} />
+            </div>
           );
         }) }
+      </div>
+      <div className="container mt-3 transactions">
+        <h1>Transactions inside block{clickedIndex + 1}</h1>
+        { transaction.length? (
+          <TransactionTable queue={transaction} />
+        ) : (
+          <p>此区块不含交易信息噢！</p>
+        ) }
+
       </div>
       <div onClick={handleWarning}></div>
     </div>
